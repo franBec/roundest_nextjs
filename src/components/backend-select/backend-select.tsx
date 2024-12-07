e22bf0898f6c6ebfc63eaabccbd80abfae7ad5f8
Fran Bec <franbecvort@gmail.com>
2024-12-07 17:06:07 +0000

    basic select + form dependencies might use later

---
+++ src/components/backend-select/backend-select.tsx
@@ -0,0 +1,35 @@
+import {
+  Select,
+  SelectContent, SelectGroup,
+  SelectItem, SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const backends = [
+  { backend: "Java", url: "http://localhost:8080" },
+  { backend: "Kotlin", url: "http://localhost:8081" },
+  { backend: "Groovy", url: "http://localhost:8082" },
+];
+
+const BackendSelect = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[200px]">
+        <SelectValue placeholder="Select a backend" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Backends</SelectLabel>
+          {backends.map((backend) => (
+            <SelectItem key={backend.url} value={backend.url}>
+              {backend.backend}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default BackendSelect;

0697dc6106f5da67403ced63d244d6ab695c81fd
Fran Bec <franbecvort@gmail.com>
2024-12-07 16:00:05 +0000

    fetching two random pokemons successfully

--- orval.config.ts
+++ orval.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   roundest: {
     output: {
       mode: "split",
-      target: "src/__generated__/api/pokemons/roundestApi.ts",
+      target: "src/__generated__/api/roundest/roundestApi.ts",
       schemas: "src/__generated__/api/roundest/model",
       client: "react-query",
       mock: false,
--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -1,14 +1,54 @@
-import React from "react";
+"use client";
 import PokemonCard from "@/components/pokemon/pokemon-card";
+import { useFindAll } from "@/__generated__/api/roundest/roundestApi";
+import Loading from "@/components/v0/loading";
+import AxiosErrorAlert from "@/components/v0/axios-error-alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 
 const Vote = () => {
+  const {
+    isPending,
+    isError,
+    data: response,
+    error
+  } = useFindAll(
+    {
+      random: true,
+      pageSize: 2
+    },
+    {axios: { baseURL: process.env.NEXT_PUBLIC_API_BACKEND_JAVA }}
+  )
+
+  if (isPending) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <AxiosErrorAlert axiosError={error} />;
+  }
+
+  if(!response.data.content){
+    return <Alert>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>No content</AlertTitle>
+      <AlertDescription>
+        The response, even though is OK, it has no content.
+      </AlertDescription>
+    </Alert>
+  }
+
   return (
-    <div className="min-h-screen bg-gray-900 flex justify-center items-center">
-      <PokemonCard
-        id={705}
-        name="Sliggoo"
-        imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/705.png"
-      />
+    <div className="min-h-screen flex justify-center items-center">
+      {response.data.content.map(it => (
+        <PokemonCard
+          key={it.id}
+          id={it.id}
+          name={it.name}
+          imageUrl={it.spriteUrl}
+        />
+      ))}
+
     </div>
   );
 };
